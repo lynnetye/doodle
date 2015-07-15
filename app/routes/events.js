2e8dbbe010debe2fa7ccbfb81bd97cb465c26d4b
Lynne Tye <lynnetye@gmail.com>
2015-07-15 14:06:15 -0700

    Upgrade Ember, -cli, -data versions

--- app/routes/events.js
+++ app/routes/events.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.find('event');
+    return this.store.findAll('event');
   },
 
   setupController: function (controller, model) {
